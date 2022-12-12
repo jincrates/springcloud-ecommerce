1b11c162842d8a62c77470e05caca63a736a0106
jincrates <94jingyu@gmail.com>
2022-12-12 23:53:23 +0900

    feat: Users Microservice와 Spring Cloud Gateway 연동

--- user-service/src/main/java/me/jincrates/userservice/controller/UserController.java
+++ user-service/src/main/java/me/jincrates/userservice/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 
