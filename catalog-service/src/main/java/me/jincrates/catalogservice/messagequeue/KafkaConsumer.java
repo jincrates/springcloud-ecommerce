6771db601bb16e8ea37f5c8030cddf460480656a
jincrates <94jingyu@gmail.com>
2023-01-01 16:40:41 +0900

    feat: Catalog Service Kafka 수량 동기화

---
+++ catalog-service/src/main/java/me/jincrates/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,44 @@
+package me.jincrates.catalogservice.messagequeue;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.jincrates.catalogservice.jpa.CatalogEntity;
+import me.jincrates.catalogservice.jpa.CatalogRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final CatalogRepository catalogRepository;
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: -> {}", kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+
+        CatalogEntity entity = catalogRepository.findByProductId((String) map.get("productId"));
+        if (entity != null) {
+            entity.setStock(entity.getStock() - (Integer) map.get("gty"));
+            catalogRepository.save(entity);
+        }
+    }
+}
